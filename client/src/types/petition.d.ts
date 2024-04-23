552ffb6699e6d7de6b050fff3138984d56d448e0
tuzicomet <leo336570@gmail.com>
2024-04-23 15:02:59 +1200

    Updated the petition type to use PetitionFull fields, and added SupportTier type
    updated the petition type to the PetitionFull type, which is used by server backend. This type contains all additional fields for petitions and is used by get and edit petitions. Also added SupportTier type

--- client/src/types/petition.d.ts
+++ client/src/types/petition.d.ts
@@ -1,10 +1,22 @@
-type Petition = {
-    petitionId: number,
-    title: string,
-    categoryId: number,
-    creationDate: string,
-    ownerId: number,
-    ownerFirstName: string,
-    ownerLastName: string,
-    numberOfSupporters: number,
-}
+// type containing all fields from the server's Petition and PetitionFull types
+type PetitionFull = {
+    petitionId: number;
+    title: string;
+    categoryId: number;
+    creationDate: string;
+    ownerId: number;
+    ownerFirstName: string;
+    ownerLastName: string;
+    numberOfSupporters: number;
+    description: string;
+    moneyRaised: number;
+    supportTiers: SupportTier[];
+};
+
+// type containing all fields from the server's SupportTier and supportTierPost types
+type SupportTier = {
+    supportTierId: number;
+    title: string;
+    description: string;
+    cost: number;
+};
