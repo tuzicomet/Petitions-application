d283c9443d667f6184aec1fc5b53c4b9abaf5da5
tuzicomet <leo336570@gmail.com>
2024-05-12 02:34:42 +1200

    Updated styling for login page and added error alerts
    some styling added to the login page using MUI, and added error alerts to show the error response if login fails

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios"; // axios library for making HTTP requests
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import {
+    Button, Dialog, DialogActions, DialogContent, DialogContentText,
+    DialogTitle, Paper, TextField, TableContainer, Table, TableBody, TableHead,
+    TableRow, TableCell, Stack, Alert, AlertTitle, Snackbar, IconButton,
+    Chip, MenuItem, InputLabel, Select
+} from "@mui/material"; // Material-UI components for styling
+import CSS from 'csstype';
+import {Search} from "@mui/icons-material";
+
+// CSS properties for the card style
+// TODO: can these be moved out?
+const card: CSS.Properties = {
+    padding: "10px",
+    margin: "20px",
+};
 
 // Login functional component
 const Login = () => {
     // Define state variables using useState hook, which are used to store and
     // manage data within React components
+    const [errorFlag, setErrorFlag] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState("");
+
     const [email, setEmail] = useState(""); //email input
     const [password, setPassword] = useState(""); // password input
     const [error, setError] = useState<string | null>(null); // error messages
@@ -31,42 +49,74 @@ const Login = () => {
                 console.error("Login failed:", error);
                 // if the response had an error message
                 if (error.response && error.response.data) {
-                    setError(error.response.data.message);
+                    //setError(error.response.data.message);
+                    setErrorFlag(true);
+                    setErrorMessage(error.toString());
                 } else { // if not, just set a generic error message
-                    setError("An unknown error occurred");
+                    //setError("An unknown error occurred");
+                    setErrorFlag(true);
+                    setErrorMessage(error.toString());
+
                 }
             });
     };
 
     return (
         <div>
-            {/* login form */}
-            <form
-                onSubmit={(e) => { // Handle form submission
-                    e.preventDefault(); // Prevent default form submission behavior
-                    loginUser(); // Call loginUser function to handle login
-                }}
-            >
-                {/* Email input field */}
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} // Update email state on input change
-                />
-                {/* Password input field */}
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} // Update password state on input change
-                />
-                {/* Submit button */}
-                <button type="submit">Login</button>
-            </form>
+            {/* Show error Alert if errorFlag is true */}
+            {errorFlag &&
+                <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    {errorMessage}
+                </Alert>
+            }
 
-            {/* Display error message if error is true */}
-            {error && <p>{error}</p>}
+            {/* Petition table section */}
+            <Paper elevation={3} style={card}>
+
+                {/* Page Title */}
+                <h1>Login</h1>
+
+                {/* login form */}
+                <form
+                    // handle form submission
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        loginUser(); // Call loginUser function to handle login
+                    }}
+                >
+                    {/* Email input field */}
+                    <div id="email-input-field">
+                        <TextField
+                            label="Email"
+                            type="email"
+                            placeholder="example@email.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} // Update email state on input change
+                        />
+                    </div>
+
+                    {/* Password input field */}
+                    <div id="password-input-field">
+                        <TextField
+                            label="Password"
+                            type="password"
+                            placeholder="..."
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} // Update password state on input change
+                        />
+                    </div>
+
+                    {/* Submit button */}
+                    <div id="submit-button">
+                        <Button variant="outlined" type="submit">
+                            Login
+                        </Button>
+                    </div>
+                </form>
+
+            </Paper>
+
         </div>
     );
 };
