27571b59aec803a70b6d7c296c1657873b87a770
tuzicomet <leo336570@gmail.com>
2024-05-12 06:58:43 +1200

    Navbar now changes what it shows based on whether client is logged in
    profile and logout buttons are only shown if client is logged in, register and login buttons are only shown if they aren't. Home and Petition buttons are still visible regardless.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,6 +5,24 @@ import {Button} from "@mui/material";
 // Navigation bar component
 // to be added to the top of each page
 const Navbar = () => {
+    // boolean saying whether the client is logged in
+    const [clientIsLoggedIn, setClientIsLoggedIn] = React.useState(false);
+
+    React.useEffect(() => {
+        const checkIfUserIsLoggedIn = () => {
+            const authToken = localStorage.getItem("savedAuthToken");
+            if (authToken) {
+                // If an auth token is found, the user is logged in
+                setClientIsLoggedIn(true);
+            } else {
+                // Otherwise, user is not logged in
+                setClientIsLoggedIn(false);
+            }
+        }
+
+        checkIfUserIsLoggedIn();
+    })
+
     return (
         <nav>
             <div id="navigation-bar">
@@ -14,18 +32,32 @@ const Navbar = () => {
                 <Button>
                     <Link to="/petitions">Petitions</Link>
                 </Button>
-                <Button>
-                    <Link to="/profile">Profile</Link>
-                </Button>
-                <Button>
-                    <Link to="/register">Register</Link>
-                </Button>
-                <Button>
-                    <Link to="/login">Login</Link>
-                </Button>
-                <Button>
-                    <Link to="/logout">Logout</Link>
-                </Button>
+
+                {/* Only show if client is logged in */}
+                {clientIsLoggedIn && (
+                    <Button>
+                        <Link to="/profile">Profile</Link>
+                    </Button>
+                )}
+
+                {/* Only show if client is not logged in */}
+                {!clientIsLoggedIn && (
+                    <>
+                        <Button>
+                            <Link to="/register">Register</Link>
+                        </Button>
+                        <Button>
+                            <Link to="/login">Login</Link>
+                        </Button>
+                    </>
+                )}
+
+                {/* Only show if client is logged in */}
+                {clientIsLoggedIn && (
+                    <Button>
+                        <Link to="/logout">Logout</Link>
+                    </Button>
+                )}
             </div>
         </nav>
     );
